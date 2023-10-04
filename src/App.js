640a33d7f3d171340f8aff5bda8dd46c98b25740
santiago <saantiagovelo@gmail.com>
2023-10-04 15:03:45 -0300

    preentrega

--- src/App.js
+++ src/App.js
@@ -23,3 +23,54 @@ function App() {
 }
 
 export default App;
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from './ItemListContainer'; // Importa el componente ItemListContainer
+import './App.css'; // Importa tus estilos personalizados aquí
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="#">Brand - Sneaklo</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Categorías
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Categoría 1</a></li>
+                  <li><a className="dropdown-item" href="#">Categoría 2</a></li>
+                  <li><a className="dropdown-item" href="#">Categoría 3</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <ItemListContainer /> {/* Agrega el componente ItemListContainer */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
